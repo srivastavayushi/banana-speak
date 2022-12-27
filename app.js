48efbb6d7a2dbcff8b04915a8483fb131499271b
ayushi-srivastava18 <ayushi.srivastava18@student.amity.edu>
2022-12-27 17:30:48 +0530

    code : input value in textarea

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-console.log("hello world");
+// console.log("hello world")
+// var text = prompt("What is your name?")
+// alert(`Helloo ${text}`);
 
-var text = prompt("What is your name?");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 
-alert(`Helloo ${text}`);
+function clickEventHandler() {
+    console.log(txtInput.value);
+};
+btnTranslate.addEventListener("click", clickEventHandler)
+
+// console.log(btnTranslate);
+
+// API - Application Programming Interface - when user talks to an application 
+// concept of callback - when an event happens do this (implement this function)
+// DOM - Modeling entire document as an object. Every element on the page is represented as a property of an object
+
+
 
